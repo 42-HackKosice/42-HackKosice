86c430096fceed7db7a0bfdc87276e83e060aad1
Matej Mazúr <41269745+MatejMa2ur@users.noreply.github.com>
2021-06-01 21:15:49 +0200

    InitialOfPoetry

---
+++ Shared/Poet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Poet
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string[] lines { get; set; }
+        public string status { get; set; }
+    }
+}
