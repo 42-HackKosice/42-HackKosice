7d200aa22942bcfbf48e0f95a3d5f20d4076b400
Matej Mazúr <41269745+MatejMa2ur@users.noreply.github.com>
2021-06-03 00:51:27 +0200

    Big Changes

--- Api/PoetResult.cs
+++ Api/PoetResult.cs
@@ -7,11 +7,17 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Globalization;
+using Azure;
+using Azure.AI.TextAnalytics;
 
 namespace BlazorApp.Api
 {
     public static class PoetResult
     {
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("d1764c4fa6514255b4b6fc39c9c04e98");
+        private static readonly Uri endpoint = new Uri("https://poetrycheck.cognitiveservices.azure.com/");
         [FunctionName("PoetResult")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
@@ -19,18 +25,49 @@ namespace BlazorApp.Api
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var client = new TextAnalyticsClient(endpoint, credentials);
+
             string text = req.Query["text"];
+            string output = "";
+            Console.WriteLine(text);
+            Console.WriteLine(text.Replace("\n", " "));
+            if (text != null)
+            {
+                text = text.Replace("\n"," ").Replace(",,",",");
+                DocumentSentiment documentSentiment = client.AnalyzeSentiment(text);
+                output += documentSentiment.Sentiment + ";";
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                var topic = client.ExtractKeyPhrases(text);
+                foreach (string phrase in topic.Value)
+                    output += $"{phrase},";
+                output.Remove(output.Length-1);
+                output += ";";
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                text = text.Replace(".","").Replace(",","").Replace("!", "").Replace("?", "").Replace("'", "");
+                text = text.ToLower();
+                Dictionary<string, int> roznorodostSlov = new Dictionary<string, int>();
+                foreach(string item in text.Split(" "))
+                {
+                    if (roznorodostSlov.ContainsKey(item))
+                        roznorodostSlov[item] += 1;
+                    else
+                        roznorodostSlov.Add(item, 1);
+                }
+                output += roznorodostSlov.Count / (text.Split(" ").Length * 1.0) * 100.0 + "%;";
 
-            return new OkObjectResult(responseMessage);
+                text = text.Replace(" ", "");
+                Dictionary<char, int> kvalita = new Dictionary<char, int>();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if(!kvalita.ContainsKey(text[i]))
+                        kvalita.Add(text[i],1);
+                    else
+                        kvalita[text[i]]+=1;
+                }
+                output += kvalita.Count/26.0*100.0+"%";
+            }
+
+            return new OkObjectResult(output);
         }
     }
 }
